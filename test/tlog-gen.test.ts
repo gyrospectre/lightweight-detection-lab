be09f5d91d7bc18261fbf254afa9f2f4b758fd82
gyrospectre <you@example.com>
2023-02-04 10:05:05 +1100

    Initial commit

---
+++ test/tlog-gen.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TlogGen from '../lib/tlog-gen-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tlog-gen-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TlogGen.TlogGenStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
